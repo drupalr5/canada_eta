cab0c0b00a94a6936f1c1ffbe56ccaff8b09f24f
Yogita <yogita.r@cisinlabs.com>
2023-04-12 17:53:38 +0530

    Login api

--- client/src/Components/ManageTeam.js
+++ client/src/Components/ManageTeam.js
@@ -1,81 +1,96 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import PageHeading from "./PageHeading";
 
 function ManageTeam(props) {
+  const [user, setUser] = useState(null);
+  let id = localStorage.getItem("id");
+  useEffect(() => {    
+    // axios.get(`http://localhost:3001/api/admin/get/${id}`).then((response) => {
+    //   setUser(response.data)
+    // }).catch((error) => {
+    //   alert(error);
+    // });
+  },[]);
+  const updateHandler = () => {
+    // axios.get(`http://localhost:3001/api/admin/update/${id}`,{}).then((response) => {
+    //   setUser(response.data)
+    // }).catch((error) => {
+    //   alert(error);
+    // });
+  }
   return (
     <>
-      <div class="row clearfix">
-        <div class="col-lg-12 col-md-12 col-sm-12">
-          <div class="card">
-            <div class="header">
-            <PageHeading pagename={props.heading}/>
+      <div className="row clearfix">
+        <div className="col-lg-12 col-md-12 col-sm-12">
+          <div className="card">
+            <div className="header">
+              <PageHeading pagename={props.heading} />
             </div>
-            <div class="body">
+            <div className="body">
               <form
-                enctype="multipart/form-data"
-                method="post"
-                action="manage_team.php?action=save"
+              encType="multipart/form-data"
+              onSubmit={updateHandler}
               >
-                <div class="row clearfix">
-                  <div class="col-md-6">
+                <div className="row clearfix">
+                  <div className="col-md-6">
                     <label>
                       <strong>Name</strong>
                     </label>
-                    <div class="form-group">
+                    <div className="form-group">
                       <input
                         type="text"
                         name="name"
-                        class="form-control"
+                        className="form-control"
                         placeholder="Enter Name"
-                        value=""
+                        // value=""
                         required
                       />
                     </div>
                   </div>
-                  <div class="col-md-6">
+                  <div className="col-md-6">
                     <label>
                       <strong>Email ID</strong>
                     </label>
-                    <div class="form-group">
+                    <div className="form-group">
                       <input
                         type="text"
                         name="email"
-                        class="form-control"
-                        value=""
+                        className="form-control"
+                        // value=""
                         placeholder="Enter Email"
                         required
-                        autocomplete="false"
+                        autoComplete="false"
                       />
                     </div>
                   </div>
                 </div>
-                <div class="row clearfix">
-                  <div class="col-md-6">
+                <div className="row clearfix">
+                  <div className="col-md-6">
                     <label>
                       <strong>Password</strong>
                     </label>
-                    <div class="form-group">
+                    <div className="form-group">
                       <input
                         type="password"
-                        class="form-control required"
+                        className="form-control required"
                         id="password"
                         name="password"
                         placeholder="Enter Password"
-                        autocomplete="false"
+                        autoComplete="false"
                       />
                     </div>
                   </div>
-                  <div class="col-md-6">
+                  <div className="col-md-6">
                     <label>
                       <strong>Member Type</strong>
                     </label>
-                    <div class="form-group">
+                    <div className="form-group">
                       <select
                         name="type"
-                        class="form-control"
+                        className="form-control"
                         required
-                        style={{height: '46px'}}
+                        style={{ height: "46px" }}
                       >
                         <option value="">Select Type</option>
                         <option value="Team">Team</option>
@@ -85,15 +100,15 @@ function ManageTeam(props) {
                     </div>
                   </div>
                 </div>
-                <div class="row clearfix">
-                  <div class="col-md-6">
+                <div className="row clearfix">
+                  <div className="col-md-6">
                     <label>
                       <strong>Upload Profile Picture</strong>
                     </label>
-                    <div class="form-group">
+                    <div className="form-group">
                       <input
                         type="file"
-                        class="form-control required"
+                        className="form-control required"
                         id="filename"
                         name="filename"
                         accept="image/*"
@@ -101,10 +116,10 @@ function ManageTeam(props) {
                     </div>
                   </div>
                 </div>
-                <div class="row clearfix">
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <button class="btn btn-primary" type="submit">
+                <div className="row clearfix">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <button className="btn btn-primary" type="submit">
                         Submit
                       </button>
                     </div>
