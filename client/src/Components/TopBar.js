6e959e6d80de993871433b83a3c87aad97c6eec0
Yogita <yogita.r@cisinlabs.com>
2023-04-13 14:29:26 +0530

    component changes

--- client/src/Components/TopBar.js
+++ client/src/Components/TopBar.js
@@ -2,7 +2,7 @@ import React, { useState,useEffect } from "react";
 import axios from "axios";
 import config from "../config.json"
 import NewOrderImage from "../Allassets/assets/images/new_order.svg";
-import NewOrderImage from "../Allassets/assets/images/new_order.svg";
+import PendingOrderImage from "../Allassets/assets/images/pending_order.svg";
 import CompletedOrderImage from "../Allassets/assets/images/completed_order.svg";
 import ContactOrderImage from "../Allassets/assets/images/contact_customer.svg";
 
@@ -44,7 +44,7 @@ function TopBar() {
             <a href="priority-order">
               <div className="body">
                 <p className="m-b-20">
-                  <img src={NewOrderImage} alt="Priority Orders"/>
+                  <img src={PendingOrderImage} alt="Priority Orders"/>
                 </p>
                 <span>Priority Orders</span>
                 <h3
--- client/src/Layout/ProtectedLayout.js
+++ client/src/Layout/ProtectedLayout.js
@@ -8,14 +8,15 @@ export const ProtectedLayout = ({ children }) => {
     if (!localStorage.getItem("isLoggedIn")) {
       return navigate("/");
     } else {
+      console.log(user.type)
       if (user.type === 'Admin') {
-        return navigate("admin/dashboard");
+        // return navigate("admin/dashboard");
       } else if (user.type === 'Team') {
         return navigate("team/dashboard");
       } else if (user.type === 'Night Staff') {
         return navigate("staff/dashboard");
       }
     }
-  }, [localStorage.getItem("user")]);
+  }, []);
   return children;
 };
